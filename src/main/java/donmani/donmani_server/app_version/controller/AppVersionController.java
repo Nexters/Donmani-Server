ba523eae0c522806b4ad087f68791c9123882369
k906506 <k906506@naver.com>
2025-03-23 16:51:27 +0900

    [fix] API Reponse 변경

--- src/main/java/donmani/donmani_server/app_version/controller/AppVersionController.java
+++ src/main/java/donmani/donmani_server/app_version/controller/AppVersionController.java
@@ -1,25 +1,26 @@
 package donmani.donmani_server.app_version.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import donmani.donmani_server.app_version.entity.PlatformType;
 import donmani.donmani_server.app_version.dto.AppVersionDTO;
 import donmani.donmani_server.app_version.service.AppVersionService;
+import donmani.donmani_server.common.httpStatus.HttpStatusDTO;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api2/appVersion")
 @RequiredArgsConstructor
 public class AppVersionController {
 	private final AppVersionService appVersionService;
 
-	@GetMapping("/{platformType}")
-	public ResponseEntity<AppVersionDTO> getLatestVersion(@PathVariable PlatformType platformType) {
-		AppVersionDTO appVersionDTO = appVersionService.getLatestVersionByPlatform((platformType));
-		return ResponseEntity.ok(appVersionDTO);
+	@GetMapping("api/v1/appVersion/{platformType}")
+	public ResponseEntity<HttpStatusDTO<AppVersionDTO>> getLatestVersion(@PathVariable PlatformType platformType) {
+		AppVersionDTO appVersion = appVersionService.getLatestVersionByPlatform((platformType));
+		return ResponseEntity.ok(
+			HttpStatusDTO.response(HttpStatus.OK.value(), "성공", appVersion));
 	}
 }
