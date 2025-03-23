bed31b0222467248acba64aefa91a5d70ccb389c
k906506 <k906506@naver.com>
2025-03-23 16:52:20 +0900

    Revert "[fix] API Reponse 변경"
    This reverts commit ba523eae0c522806b4ad087f68791c9123882369.

--- src/main/java/donmani/donmani_server/expense/controller/ExpenseController.java
+++ src/main/java/donmani/donmani_server/expense/controller/ExpenseController.java
@@ -1,6 +1,5 @@
 package donmani.donmani_server.expense.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,51 +9,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import donmani.donmani_server.common.httpStatus.HttpStatusDTO;
 import donmani.donmani_server.expense.dto.ExpenseRequestDTO;
 import donmani.donmani_server.expense.dto.ExpenseResponseDTO;
 import donmani.donmani_server.expense.service.ExpenseService;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/expenses")
 @RequiredArgsConstructor
 public class ExpenseController {
 	private final ExpenseService expenseService;
 
-	@GetMapping("api/v1/expenses/calendar/{userKey}")
-	public ResponseEntity<HttpStatusDTO<ExpenseResponseDTO>> getExpensesCalendar(
+	@GetMapping("/calendar/{userKey}")
+	public ResponseEntity<ExpenseResponseDTO> getExpensesCalendar(
 		@PathVariable String userKey,
 		@RequestParam int year,
 		@RequestParam int month) {
-		try {
-			ExpenseResponseDTO response = expenseService.getExpenses(userKey, year, month, false);
-			return ResponseEntity.ok(HttpStatusDTO.response(HttpStatus.OK.value(), "성공", response));
-		} catch (IllegalArgumentException e) {
-			return ResponseEntity.ok(
-				HttpStatusDTO.response(HttpStatus.INTERNAL_SERVER_ERROR.value(), "유저 정보 없음", null));
-		}
+		ExpenseResponseDTO response = expenseService.getExpenses(userKey, year, month, false);
+		return ResponseEntity.ok(response);
 	}
 
-	@GetMapping("api/v1/expenses/list/{userKey}")
-	public ResponseEntity<HttpStatusDTO<ExpenseResponseDTO>> getExpensesList(
-		@PathVariable String userKey,
-		@RequestParam int year,
-		@RequestParam int month) {
-		try {
-			ExpenseResponseDTO response = expenseService.getExpenses(userKey, year, month, true);
-			return ResponseEntity.ok(HttpStatusDTO.response(HttpStatus.OK.value(), "성공", response));
-		} catch (IllegalArgumentException e) {
-			return ResponseEntity.ok(
-				HttpStatusDTO.response(HttpStatus.INTERNAL_SERVER_ERROR.value(), "유저 정보 없음", null));
-		}
+	@GetMapping("/list/{userKey}")
+	public ResponseEntity<ExpenseResponseDTO> getExpensesList(
+			@PathVariable String userKey,
+			@RequestParam int year,
+			@RequestParam int month) {
+		ExpenseResponseDTO response = expenseService.getExpenses(userKey, year, month, true);
+		return ResponseEntity.ok(response);
 	}
 
-	@PostMapping("api/v1/expenses")
-	public ResponseEntity<HttpStatusDTO<Void>> addExpense(@RequestBody ExpenseRequestDTO request) {
+	@PostMapping
+	public ResponseEntity<Void> addExpense(@RequestBody ExpenseRequestDTO request) {
 		expenseService.addExpense(request);
-
-		return ResponseEntity.ok(
-			HttpStatusDTO.response(HttpStatus.CREATED.value(), "성공", null));
+		return ResponseEntity.ok().build();
 	}
 }
