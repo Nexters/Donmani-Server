e78d660b90fc54ee79f3bba918930fbfbf3b8e4b
k906506 <k906506@naver.com>
2025-04-16 00:10:40 +0900

    [fix] Annotation 수정 #49

--- src/main/java/donmani/donmani_server/user/dto/UserRegisterResponseDTO.java
+++ src/main/java/donmani/donmani_server/user/dto/UserRegisterResponseDTO.java
@@ -2,11 +2,12 @@ package donmani.donmani_server.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserRegisterResponseDTO {
-	// private boolean isNew;
 	private String userKey;
 	private String userName;
 }
