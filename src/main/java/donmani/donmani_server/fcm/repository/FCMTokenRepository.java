ee494333e814a0cb3308e66ca499d4ebc072937e
k906506 <k906506@naver.com>
2026-02-16 10:13:06 +0900

    [feat] 운세 발송 스케쥴 및 방어 로직 추가 #131

--- src/main/java/donmani/donmani_server/fcm/repository/FCMTokenRepository.java
+++ src/main/java/donmani/donmani_server/fcm/repository/FCMTokenRepository.java
@@ -1,8 +1,11 @@
 package donmani.donmani_server.fcm.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import donmani.donmani_server.fcm.entity.FCMToken;
@@ -11,4 +14,22 @@ import donmani.donmani_server.user.entity.User;
 @Repository
 public interface FCMTokenRepository extends JpaRepository<FCMToken, Long> {
 	Optional<FCMToken> findByUser(User user);
+
+	@Query(value = "\n"
+		+ "SELECT token\n"
+		+ "FROM fcmtoken\n"
+		+ "WHERE 1=1\n"
+		+ "AND token NOT IN (\n"
+		+ "  SELECT a.token\n"
+		+ "  FROM fcmtoken a\n"
+		+ "  LEFT JOIN fortune_histories b ON a.user_id = b.user_id\n"
+		+ "    AND DATE_FORMAT(b.created_at, '%Y%m%d') = :target_date\n"
+		+ "  LEFT JOIN fcm_logs d ON a.user_id = d.user_id\n"
+		+ "    AND DATE_FORMAT(d.created_at, '%Y%m%d') = :target_date\n"
+		+ "    AND d.notification_type = 'FORTUNE'\n"
+		+ "    AND d.status            = 'SUCCESS'\n"
+		+ "  JOIN fortune c ON b.fortune_id = c.id\n"
+		+ "    AND c.target_date = :target_date\n"
+		+ ")\n", nativeQuery = true)
+	List<String> findAllTokensToSendFortune(LocalDate localDate);
 }
